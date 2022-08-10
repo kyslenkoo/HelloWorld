3129d96cabcac65b00e141d7ab6fdea57b5b2a16
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-08-10 18:46:26 +0300

    concatenation

---
+++ 10.08.22/js/script.js
@@ -0,0 +1,10 @@
+console.log('hello'); // строки берем в кавычки
+console.log('world');
+console.log(`hello world`);
+console.log('3333');
+console.log(334);
+console.log("hello world");
+console.log('hello' + "world"); // конкатенация
+console.log('hello' + ' world'); // конкатенация
+console.log('hello ' + 'world'); // конкатенация
+console.log('hello' + ' ' + 'world'); // конкатенация
